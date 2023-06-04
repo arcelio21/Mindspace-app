d15f71821bba024b9495e42663d4068f82371d5b
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-06-04 00:34:47 -0500

    Se termino funcionalidad de registro y se agrego observable para esperar respuesta de authentificacion

--- app/src/main/java/com/mindspace/app/usecases/registrarse/RegistrarseActivity.java
+++ app/src/main/java/com/mindspace/app/usecases/registrarse/RegistrarseActivity.java
@@ -1,17 +1,32 @@
 package com.mindspace.app.usecases.registrarse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.mindspace.app.model.user.AuthenticationUser;
 import com.mindspace.app.usecases.login.LoginActivity;
 import com.mindspace.app.R;
 
-public class Registrarse extends AppCompatActivity {
+public class RegistrarseActivity extends AppCompatActivity {
+
+    private Button btnRegister;
+    private Button btnBackLogin;
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etName;
+    private EditText etApellido;
+    private EditText etEdad;
+    private RegisterViewModel registerViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +43,77 @@ public class Registrarse extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY );
         }
 
+        this.registerViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
 
-        Button buttonRegister = findViewById(R.id.buttonReturnLogin);
-        buttonRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Acción a realizar al hacer clic en el botón "ya tengo cuenta"
-                // Por ejemplo, iniciar una nueva actividad
-                Intent intent = new Intent(Registrarse.this, LoginActivity.class);
-                startActivity(intent);
+        this.etEmail = findViewById(R.id.etEmai);
+        this.etPassword = findViewById(R.id.etPassword);
+        this.etName = findViewById(R.id.etNOmbre);
+        this.etApellido = findViewById(R.id.etApellido);
+        this.etEdad = findViewById(R.id.etEdad);
+
+        this.btnBackLogin = findViewById(R.id.buttonReturnLogin);
+        this.setListenerButtonBackLogin();
+
+        this.btnRegister = findViewById(R.id.buttonRegistrarme);
+        this.setListenerButtonRegistrarme();
+
+        this.setListenerResponseAuthentication();
+
+
+    }
+
+    private void setListenerButtonBackLogin(){
+        this.btnBackLogin.setOnClickListener(view -> {
+            // Acción a realizar al hacer clic en el botón "ya tengo cuenta"
+            // Por ejemplo, iniciar una nueva actividad
+            Intent intent = new Intent(RegistrarseActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    //TODO CAMBIAR IMPLEMENTACION DE OBTENCION DE DATOS DE USUAIORS
+    private void setListenerButtonRegistrarme(){
+
+        this.btnRegister.setOnClickListener(view ->{
+
+            String email = this.etEmail.getText().toString();
+            String password = this.etPassword.getText().toString();
+
+            if(!email.trim().isEmpty() && !password.trim().isEmpty()){
+
+                String nombre = this.etName.getText().toString();
+                String apellido = this.etApellido.getText().toString();
+                //TODO MANEJAR EXEPCION
+                Integer edad = Integer.valueOf(this.etEdad.getText().toString());
+
+                if(!nombre.trim().isEmpty() && !apellido.trim().isEmpty() && edad>0){
+
+                    AuthenticationUser user = AuthenticationUser.builder()
+                            .email(email)
+                            .password(password)
+                            .build();
+
+                    this.registerViewModel.createAccount(user);
+
+                }else {
+                    Toast.makeText(this,"Ingrese los datos completos", Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(this,"Ingrese un correo electronico y contraseña", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+
+    private void setListenerResponseAuthentication(){
+        this.registerViewModel.getResponseAuthenticacion().observe(this, responseAuth -> {
+            if(responseAuth){
+                Intent test = new Intent(this, LoginActivity.class);
+                startActivity(test);
+            }else {
+                Toast.makeText(this,"Correo o contraseña no valido", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
