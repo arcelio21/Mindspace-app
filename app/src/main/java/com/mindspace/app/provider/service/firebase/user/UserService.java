1194f9433a3080640caeb432d605dc2d4e9694bf
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-06-04 00:35:23 -0500

    Servicio para authentificacion de usuario

---
+++ app/src/main/java/com/mindspace/app/provider/service/firebase/user/UserService.java
@@ -0,0 +1,107 @@
+package com.mindspace.app.provider.service.firebase.user;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.mindspace.app.model.user.AuthenticationUser;
+import com.mindspace.app.model.user.UsuarioPost;
+import com.mindspace.app.usecases.base.ListenerAuthentication;
+
+import java.util.Map;
+
+public class UserService {
+
+    private final FirebaseAuth authentication;
+    private final FirebaseFirestore baseDatos;
+
+    private ListenerAuthentication listenerAuth;
+
+    public UserService(ListenerAuthentication authentication) {
+        this.baseDatos = FirebaseFirestore.getInstance();
+        this.authentication = FirebaseAuth.getInstance();
+        this.listenerAuth=authentication;
+    }
+
+
+    public void creacionCuenta(AuthenticationUser authenticationUser){
+
+
+        if(authenticationUser==null
+            ||authenticationUser.getEmail()==null ||authenticationUser.getEmail().trim().isEmpty()
+            || authenticationUser.getPassword()==null ||authenticationUser.getPassword().trim().isEmpty()
+        ){
+            this.listenerAuth.notifyResponse(false);
+            return;
+        }
+
+        Task<AuthResult> response = this.authentication.createUserWithEmailAndPassword(
+                authenticationUser.getEmail(), authenticationUser.getPassword()
+        );
+
+        response.addOnCompleteListener(task -> {
+            this.listenerAuth.notifyResponse(task.isSuccessful());
+        });
+    }
+
+    /**
+     * Inicia sesión de usuario.
+     *
+     * @param authenticationUser El objeto AuthenticationUser con los datos de autenticación del usuario.
+     * @return true si el inicio de sesión es exitoso, false de lo contrario.
+     */
+    public void login(AuthenticationUser authenticationUser){
+
+        if(authenticationUser==null
+                ||authenticationUser.getEmail()==null ||authenticationUser.getEmail().trim().isEmpty()
+                || authenticationUser.getPassword()==null ||authenticationUser.getPassword().trim().isEmpty()
+        ){
+            this.listenerAuth.notifyResponse(false);
+            return;
+        }
+
+
+        this.authentication.
+                signInWithEmailAndPassword(authenticationUser.getEmail(), authenticationUser.getPassword())
+                .addOnCompleteListener(task -> {
+                    this.listenerAuth.notifyResponse(task.isSuccessful());
+                });
+
+    }
+
+    /**
+     * Guarda un objeto UsuarioPost en la base de datos.
+     *TODO PENSAR EN PONERLO VOID
+     * @param usuarioPost El objeto UsuarioPost a guardar.
+     * @return true si se guarda exitosamente, false de lo contrario.
+     */
+    public Boolean save(UsuarioPost usuarioPost){
+
+        if(usuarioPost==null
+             || usuarioPost.getEmail()==null || usuarioPost.getEmail().trim().isEmpty()
+             || usuarioPost.getNombre()==null || usuarioPost.getNombre().trim().isEmpty()
+             || usuarioPost.getApellido()==null || usuarioPost.getApellido().trim().isEmpty()
+             || usuarioPost.getEdad()==null || usuarioPost.getEdad()<=0
+        ){
+            return  false;
+        }
+
+        Task<Void> response = this.baseDatos.collection("usuarios")
+                .document(usuarioPost.getEmail())
+                .set(this.usuarioPostToMapToSave(usuarioPost));
+        return response.isSuccessful();
+    }
+
+    /**
+     * Convierte un objeto UsuarioPost a un Map<String, Object> para guardar en la base de datos.
+     *
+     * @param user El objeto UsuarioPost a convertir.
+     * @return Un Map con los datos del usuario en el formato requerido para la base de datos.
+     */
+    private Map<String, Object> usuarioPostToMapToSave(UsuarioPost user){
+        return Map.of("nombre", user.getNombre(),
+                "apellido", user.getApellido(),
+                "edad", user.getEdad());
+
+    }
+}
