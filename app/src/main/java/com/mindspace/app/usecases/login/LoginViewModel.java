f4e3383344e47b10196801c8d737ed1755cf5111
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-06-04 00:51:14 -0500

    Documentacion de metodos de viewmodel de Login

--- app/src/main/java/com/mindspace/app/usecases/login/LoginViewModel.java
+++ app/src/main/java/com/mindspace/app/usecases/login/LoginViewModel.java
@@ -1,6 +1,5 @@
 package com.mindspace.app.usecases.login;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -10,6 +9,9 @@ import com.mindspace.app.provider.service.firebase.user.UserService;
 import com.mindspace.app.usecases.base.ListenerAuthentication;
 import com.mindspace.app.util.Encrypt;
 
+/**
+ * ViewModel para el inicio de sesión.
+ */
 public class LoginViewModel extends ViewModel implements ListenerAuthentication {
 
     private UserService userService;
@@ -19,6 +21,11 @@ public class LoginViewModel extends ViewModel implements ListenerAuthentication 
         this.userService = new UserService(this);
     }
 
+    /**
+     * Realiza el inicio de sesión del usuario.
+     *
+     * @param user Objeto AuthenticationUser que contiene la información de autenticación del usuario.
+     */
     public void login(AuthenticationUser user){
 
         AuthenticationUser userFinal =AuthenticationUser.builder()
@@ -29,18 +36,35 @@ public class LoginViewModel extends ViewModel implements ListenerAuthentication 
         this.userService.login(userFinal);
     }
 
+
+    /**
+     * Obtener datos del usuario.
+     *
+     * @param email Email del usuario para obtener sus datos.
+     * @return Objeto UsuarioGet con los datos del usuario.
+     */
     public  UsuarioGet getUser(String email){
         email = Encrypt.encrypt(email);
         //TODO SE APLICA LOGICA PARA OBETNEER USUARIO
         return null;
     }
 
+    /**
+     * Método de notificación de respuesta de autenticación.
+     *
+     * @param response Respuesta de autenticación.
+     */
     @Override
     public void notifyResponse(Boolean response) {
         this.responseAuthenticacion.postValue(response);
     }
 
 
+    /**
+     * Obtener el LiveData de respuesta de autenticación.
+     *
+     * @return LiveData<Boolean> con la respuesta de autenticación.
+     */
     public MutableLiveData<Boolean> getResponseAuthenticacion() {
         return responseAuthenticacion;
     }
