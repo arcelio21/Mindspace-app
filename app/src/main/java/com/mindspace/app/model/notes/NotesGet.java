40dfc99dc2876027880ead07e5fea948cae8c9b7
derek vargas <vargasdarek123@gmail.com>
2023-06-11 22:08:59 -0500

    Se añadio el atributo id de notas y las anotaciones.

--- app/src/main/java/com/mindspace/app/model/notes/NotesGet.java
+++ app/src/main/java/com/mindspace/app/model/notes/NotesGet.java
@@ -1,4 +1,13 @@
 package com.mindspace.app.model.notes;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Builder
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class NotesGet {
+    Long ID;
 }
