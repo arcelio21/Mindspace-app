90f6b786bd1780e9e1d81482b9863d4178e891d9
Zeth507 <cubillajohan0@gmail.com>
2023-06-13 23:20:02 -0500

    anexos de fuentes e imagenes

---
+++ app/src/main/java/com/mindspace/app/usecases/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.mindspace.app.usecases.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mindspace.app.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
