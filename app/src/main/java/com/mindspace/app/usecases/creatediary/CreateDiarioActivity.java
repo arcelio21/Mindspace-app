b203bfb6030258310be29c644914761e1dfaf9d8
derek vargas <vargasdarek123@gmail.com>
2023-07-12 14:13:49 -0500

    Se creo el activity y se vinculo al activity_crearnota con sus metodos override.

---
+++ app/src/main/java/com/mindspace/app/usecases/creatediary/CreateDiarioActivity.java
@@ -0,0 +1,18 @@
+package com.mindspace.app.usecases.creatediary;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mindspace.app.R;
+
+public class CreateDiarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crearnota);
+    }
+}
+
