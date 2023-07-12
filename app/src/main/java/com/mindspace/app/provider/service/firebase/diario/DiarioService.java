7c81f91bc99749e008f1f725d033cb3081857f8a
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-12 11:24:14 -0500

    Creacion de metodo para validar existencia de email de usuario autentificado

--- app/src/main/java/com/mindspace/app/provider/service/firebase/diario/DiarioService.java
+++ app/src/main/java/com/mindspace/app/provider/service/firebase/diario/DiarioService.java
@@ -2,15 +2,8 @@ package com.mindspace.app.provider.service.firebase.diario;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.mindspace.app.model.user.DiarioPost;
-import com.mindspace.app.model.user.UsuarioPost;
-import com.mindspace.app.usecases.base.ListenerAuthentication;
-
-import java.util.Date;
-import java.text.SimpleDateFormat;
 
 public class DiarioService {
 
@@ -23,19 +16,38 @@ public class DiarioService {
     }
 
     public void save(DiarioPost diarioPost) {
-        if (this.authentication.getCurrentUser() == null) {
-            return;
-        }
-        String email = this.authentication.getCurrentUser().getEmail();
 
-        if (email==null||email.trim().isEmpty()){
+        String emailCurrentUser = "";
+
+        if(!this.validateEmailCurrentUser(emailCurrentUser)){
             return;
         }
+
         Task<Void> response  = baseDatos
-                .collection("usuarios").document(email)
+                .collection("usuarios").document(emailCurrentUser)
                 .collection("diarios").document().set(diarioPost);
         response.addOnSuccessListener(task->{
 
         });
+    }
+
+    /**
+     * VALIDAR EMAIL DEL USUARIO ACTUAL
+     * @param email Almacenara email del usuario actual
+     * @return response
+     */
+    private Boolean validateEmailCurrentUser(String email){
+        Boolean response = false;
+
+        if (this.authentication.getCurrentUser() == null) {
+            return response;
+        }
+        email = this.authentication.getCurrentUser().getEmail();
+
+        if (email==null||email.trim().isEmpty()){
+            return response;
+        }
+
+        return true;
     }
 }
