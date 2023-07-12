dce89ad2f1d4b5d5cb7ed2d15cafeeee5153a7ee
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-12 11:53:02 -0500

    Enlazando layout de dario con una activityclass

---
+++ app/src/main/java/com/mindspace/app/usecases/listdiario/DiarioAcitivity.java
@@ -0,0 +1,17 @@
+package com.mindspace.app.usecases.listdiario;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mindspace.app.R;
+
+public class DiarioAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diario);
+    }
+}
