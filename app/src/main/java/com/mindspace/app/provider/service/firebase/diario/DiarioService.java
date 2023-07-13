01ba18e3436a2511fb0acb5dc18d11a766ba7554
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-12 22:03:26 -0500

    Creacion de funcionalidad de obtencion de diarios, y cambio de creacion de instancia de service

--- app/src/main/java/com/mindspace/app/provider/service/firebase/diario/DiarioService.java
+++ app/src/main/java/com/mindspace/app/provider/service/firebase/diario/DiarioService.java
@@ -1,8 +1,11 @@
 package com.mindspace.app.provider.service.firebase.diario;
 
+import android.util.Log;
+
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.mindspace.app.model.user.DiarioGet;
 import com.mindspace.app.model.user.DiarioPost;
@@ -31,36 +34,67 @@ public class DiarioService {
 
     public void save(DiarioPost diarioPost) {
 
-        String emailCurrentUser = "";
+        String idCurrentUser = this.validateIdCurrentUser("");
 
-        if(!this.validateEmailCurrentUser(emailCurrentUser)){
+        if(idCurrentUser.trim().isEmpty()){
             return;
         }
 
         Task<Void> response  = baseDatos
-                .collection("usuarios").document(emailCurrentUser)
+                .collection("usuarios").document(idCurrentUser)
                 .collection("diarios").document().set(diarioPost);
         response.addOnCompleteListener(task -> {
             this.responseFirabase.notifyChange(task.isSuccessful());
         });
     }
+
+    /**
+     * OBTENER TODOS LOS DIARIOS CREADOS POR UN USUARIO
+     */
+    public void getAll(ListenerGetFirebase<DiarioGet> listener){
+        String idCurrentUser=this.validateIdCurrentUser("");
+
+        if(idCurrentUser.trim().isEmpty()){
+            return;
+        }
+
+        this.baseDatos.collection("usuarios")
+                .document(idCurrentUser)
+                .collection("diarios")
+                .get()
+                .addOnCompleteListener(query -> {
+                    Log.d("SUCCESS", query.isSuccessful()+"");
+                    if(query.isSuccessful()){
+                        Log.d("GET DIARIOS", query.isSuccessful()+"");
+                        List<DiarioGet> diarios = query.getResult().toObjects(DiarioGet.class);
+                        listener.getData(diarios);
+                    }
+                })
+                .addOnFailureListener(runnable -> {
+                    Log.d("ERROR1", runnable.getCause().getMessage());
+                    Log.d("ERROR", runnable.getMessage());
+                });
+
+
+    }
+
     /**
      * VALIDAR EMAIL DEL USUARIO ACTUAL
-     * @param email Almacenara email del usuario actual
+     * @param id Almacenara id del usuario actual
      * @return response
      */
-    private Boolean validateEmailCurrentUser(String email){
+    private String validateIdCurrentUser(String id){
         Boolean response = false;
 
         if (this.authentication.getCurrentUser() == null) {
-            return response;
+            return id;
         }
-        email = this.authentication.getCurrentUser().getEmail();
+        id = this.authentication.getCurrentUser().getUid();
 
-        if (email==null||email.trim().isEmpty()){
-            return response;
+        if (id==null||id.trim().isEmpty()){
+            return "";
         }
 
-        return true;
+        return id;
     }
 }
