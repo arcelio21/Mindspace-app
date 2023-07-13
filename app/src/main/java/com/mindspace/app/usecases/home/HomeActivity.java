12d73d819f3c5830533e375a77244f888000e5ba
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-12 22:05:09 -0500

    Se creo evento para rederigir a listar diarios

--- app/src/main/java/com/mindspace/app/usecases/home/HomeActivity.java
+++ app/src/main/java/com/mindspace/app/usecases/home/HomeActivity.java
@@ -2,15 +2,27 @@ package com.mindspace.app.usecases.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.mindspace.app.R;
+import com.mindspace.app.usecases.listdiario.DiarioAcitivity;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button btnListDiario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        this.btnListDiario = findViewById(R.id.home_btnListDiario);
+        this.btnListDiario.setOnClickListener(view -> {
+            Intent listDiarioActivity = new Intent(this, DiarioAcitivity.class);
+            startActivity(listDiarioActivity);
+        });
     }
+
+
 }
