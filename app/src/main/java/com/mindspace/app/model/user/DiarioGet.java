09cb5589dffb8e9c9241f6d4de118763b2a0262a
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-12 22:02:33 -0500

    Cambio de atributo

--- app/src/main/java/com/mindspace/app/model/user/DiarioGet.java
+++ app/src/main/java/com/mindspace/app/model/user/DiarioGet.java
@@ -2,16 +2,21 @@ package com.mindspace.app.model.user;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
  * CUANDO SE PIDA INFO AL LA BD SE UTILIZA ESTA CLASE
  */
-@Data
+@NoArgsConstructor
+@Setter
+@Getter
 public class DiarioGet {
 
     private Long id;
     private String titulo;
     private String cuerpo;
-    private  String fecha;
+    private  String fechaCreacion;
+    private String ultimaActualizacion;
 }
