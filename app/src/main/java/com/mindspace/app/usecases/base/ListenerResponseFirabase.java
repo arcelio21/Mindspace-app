6f7560f7805063b10354e5c1f53706972fb445f3
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-12 22:04:40 -0500

    Eliminacion de metodo innecesario

--- app/src/main/java/com/mindspace/app/usecases/base/ListenerResponseFirabase.java
+++ app/src/main/java/com/mindspace/app/usecases/base/ListenerResponseFirabase.java
@@ -1,12 +1,7 @@
 package com.mindspace.app.usecases.base;
 
-import com.mindspace.app.model.user.DiarioGet;
-
-import java.util.List;
-
 public interface ListenerResponseFirabase {
 
     void notifyChange(Object response);
 
-    void getData(List<?> data);
 }
