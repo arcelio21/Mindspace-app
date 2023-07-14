01dbaf101022b9f1fd7ad317e4f34ff863a3b447
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-14 12:12:06 -0500

    Elanzando boton para redirigir a perfil activity

--- app/src/main/java/com/mindspace/app/usecases/home/HomeActivity.java
+++ app/src/main/java/com/mindspace/app/usecases/home/HomeActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.mindspace.app.R;
 import com.mindspace.app.usecases.listdiario.DiarioAcitivity;
+import com.mindspace.app.usecases.perfiluser.PerfilActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Button btnListDiario;
+    private ImageButton btnPerfilUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,13 @@ public class HomeActivity extends AppCompatActivity {
         this.btnListDiario.setOnClickListener(view -> {
             Intent listDiarioActivity = new Intent(this, DiarioAcitivity.class);
             startActivity(listDiarioActivity);
+        });
+
+        this.btnPerfilUser = findViewById(R.id.Home_BtnUsuario);
+
+        this.btnPerfilUser.setOnClickListener(view -> {
+            Intent perfilActivity = new Intent(this, PerfilActivity.class);
+            startActivity(perfilActivity);
         });
     }
 
