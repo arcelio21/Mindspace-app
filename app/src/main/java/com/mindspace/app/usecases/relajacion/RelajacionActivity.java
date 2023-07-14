29c9bf29215dc7aaa0ce2f821b3df1b7ec5a55fe
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-14 16:22:24 -0500

    Evento por defecto para botones de activity relajacion

--- app/src/main/java/com/mindspace/app/usecases/relajacion/RelajacionActivity.java
+++ app/src/main/java/com/mindspace/app/usecases/relajacion/RelajacionActivity.java
@@ -1,6 +1,8 @@
 package com.mindspace.app.usecases.relajacion;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +15,10 @@ public class RelajacionActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relajacion);
+    }
+
+
+    public void listenerBtn(View view){
+        Toast.makeText(this,"Funcionalidad no diponible",Toast.LENGTH_SHORT).show();
     }
 }
