083178eafff784df2ce1f95b1107a452e03bf7ea
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-14 12:12:36 -0500

    Enlazando layout de Relacion con una activityclass

---
+++ app/src/main/java/com/mindspace/app/usecases/relajacion/RelajacionActivity.java
@@ -0,0 +1,17 @@
+package com.mindspace.app.usecases.relajacion;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mindspace.app.R;
+
+public class RelajacionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relajacion);
+    }
+}
