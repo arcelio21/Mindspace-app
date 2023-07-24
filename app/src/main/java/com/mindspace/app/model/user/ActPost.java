58ec685ffe7e5ea2fd86983002d049e2432c2a5f
Zeth507 <cubillajohan0@gmail.com>
2023-07-24 01:19:59 -0500

    Creacion del post activity_rprofunda.xml

---
+++ app/src/main/java/com/mindspace/app/model/user/ActPost.java
@@ -0,0 +1,20 @@
+package com.mindspace.app.model.user;
+
+import java.util.Date;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * CUANDO SE ENVIE DATOS A LA BD
+
+ */
+@Builder
+@Getter
+public class ActPost {
+
+    private String titulo;
+    private String cuerpo;
+
+
+}
