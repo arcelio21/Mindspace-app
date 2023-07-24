ea7fcd545a541c684521b7a2f0e80a137164c589
Zeth507 <cubillajohan0@gmail.com>
2023-07-24 01:18:18 -0500

    Creacion de la clase de relajacion profunda RprofundaActivity.java

---
+++ app/src/main/java/com/mindspace/app/usecases/relajacion/RprofundaActivity.java
@@ -0,0 +1,16 @@
+package com.mindspace.app.usecases.relajacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mindspace.app.R;
+
+public class RprofundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rprofunda);
+    }
+}
